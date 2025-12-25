ebef87d6291684de9891f897381aa677cbe19f31
sofianelamine772-design <sofianelamine772@gmail.com>
2025-12-25 11:30:11 +0100

    dd

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
-import CursorSpotlight from './CursorSpotlight';
 
 const Layout = () => {
     return (
         <div className="flex flex-col min-h-screen">
-            <CursorSpotlight />
             <Header />
             <main className="flex-grow pt-[72px]">
                 {/* pt-[72px] to account for fixed header height */}
